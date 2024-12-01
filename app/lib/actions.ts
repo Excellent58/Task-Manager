5567edf63e1cead4cd99d215afa3710944b31286
Excellent58 <wycliffemusau99@gmail.com>
2024-12-01 19:54:37 +0300

    enhanced user and task interface

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,14 +4,12 @@ import { ObjectId } from "mongodb";
 
 
 interface User {
-    _id: ObjectId;
     name: string;
     email: string;
     password: string
 }
 
 interface Task {
-    _id: ObjectId;
     title: string;
     description: string;
     isCompleted: Date
